5a8bb7ceb184e8504535ebb54b127e8cd0b4d6b1
Alex Sokol / y9san9 <y9san9@gmail.com>
2023-07-13 21:09:11 +0300

    fix: added disallow composable calls

--- compose/src/main/kotlin/app/meetacy/di/android/compose/remember.kt
+++ compose/src/main/kotlin/app/meetacy/di/android/compose/remember.kt
@@ -1,9 +1,10 @@
 package app.meetacy.di.android.compose
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisallowComposableCalls
 import app.meetacy.di.DI
 
 @Composable
-inline fun <T> DI.remember(crossinline calculation: DI.() -> T): T {
+inline fun <T> DI.remember(crossinline calculation: @DisallowComposableCalls DI.() -> T): T {
     return androidx.compose.runtime.remember { calculation() }
 }
