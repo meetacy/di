84323f5ed28172a05cbca3eb734a5fa08de3ec79
Alex Sokol / y9san9 <y9san9@gmail.com>
2023-07-18 15:41:33 +0300

    feat: refactored global di

--- android/src/main/kotlin/app.meetacy.di.android/AndroidDI.kt
+++ android/src/main/kotlin/app.meetacy.di.android/AndroidDI.kt
@@ -4,19 +4,23 @@ package app.meetacy.di.android
 
 import android.app.Application
 import app.meetacy.di.DI
-import app.meetacy.di.builder.DIBuilder
 import app.meetacy.di.builder.di
 import app.meetacy.di.dependency.Dependency
+import app.meetacy.di.android.di as globalDi
 
 lateinit var di: DI private set
 
 val DI.application: Application by Dependency
 
 public object AndroidDI {
-    public fun build(application: Application, builder: DIBuilder.() -> Unit) {
-        di = di {
+    public fun inject(
+        application: Application,
+        di: DI
+    ) {
+        if (::globalDi.isInitialized) error("DI can only be initialized once")
+
+        globalDi = di + di(checkDependencies = false) {
             val application by constant(application)
-            builder()
         }
     }
 }
